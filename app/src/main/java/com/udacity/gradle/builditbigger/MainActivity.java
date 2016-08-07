adfef9b72762e3416fdce49afa0ed4a9f8f5ca93
Akshay Deshraj <akshay30nov@gmail.com>
2016-08-07 17:43:20 +0530

    display joke when button is pressed

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.jokes.JokeProvider;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -39,8 +41,8 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+    public void tellJoke(View view) {
+        Toast.makeText(this, JokeProvider.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
